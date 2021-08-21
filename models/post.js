db162bd000d1e897bb87f22a55ad6ea2ac4fed04
Karen Cabacungan <kcabacun@uw.edu>
2021-08-21 16:37:52 -0700

    initial models code -kc

---
+++ models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    userId: { type: String, required: true, index: true },
+    dateCreated: { type: Date, required: true }
+});
+
+module.exports = mongoose.model("post", postSchema);
