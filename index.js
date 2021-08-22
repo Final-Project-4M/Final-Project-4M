918b7374fa8fb29f6f31bc1ef932dc7a5b4f2666
Giorgio Suharianto <giosuharianto@gmail.com>
2021-08-22 11:10:23 -0700

    Trying to fix npm start, but still failing

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 
+
 mongoose.connect('mongodb://localhost/Final-Project-4M', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 }).then(() => {
   server.listen(port, () => {
-   console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server is listening on http://localhost:${port}`);
   });
 });
