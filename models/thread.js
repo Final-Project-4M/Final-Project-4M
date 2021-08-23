0941dbb36d85dd70da22a889348fe92cb52cfacf
Giorgio Suharianto <giosuharianto@gmail.com>
2021-08-22 22:15:27 -0700

    Revert last commit

--- models/thread.js
+++ models/thread.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const threadSchema = new mongoose.Schema({
     posts: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'posts' }], required: true },
-    userId: { type: String, required: true, index: true }
-    // dateCreated: { type: Date, required: true, default: Date.now() }
+    userId: { type: String, required: true, index: true },
+    dateCreated: { type: Date, required: true }
 });
 
 module.exports = mongoose.model("thread", threadSchema);
