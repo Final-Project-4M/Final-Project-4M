eec0e98f812fdbdadd9d7976526b4dced0e1e282
Giorgio Suharianto <giosuharianto@gmail.com>
2021-08-22 17:21:26 -0700

    Testing without bcrypt

--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,39 @@
 const { Router } = require("express");
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const router = Router();
 
 const userDAO = require('../daos/user');
 const tokenDAO = require('../daos/token');
 
+router.get("/", (req, res, next) => {
+  res.send('<p>Hello!</p>');
+});
+
 // Signup: POST /login/signup - Uses bcrypt on the incoming password.
 // Stores users with their email and encrypted password, handles conflicts
 // when the email is already in use
-router.post("/signup", async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!password || !email) {
-    res.status(400).send('Missing signup info');
-  } else if (email === '' || password === '') {
-    res.status(400).send('Both email and password cannot be empty');
-  } else {
-    try {
-      const user = await userDAO.getUser(email);
-      if (user) {
-        res.status(409).send('Email already signed up');
-      } else {
-        const encryptedPassword = await bcrypt.hash(password, 10);
-        const userInfo = ({ email, password: encryptedPassword });
-        const savedUser = await userDAO.createUser(userInfo);
-        res.json(savedUser);
-      }
-    } catch (e) {
-      next(e);
-    }
-  }
-});
+// router.post("/signup", async (req, res, next) => {
+//   const { email, password } = req.body;
+//   if (!password || !email) {
+//     res.status(400).send('Missing signup info');
+//   } else if (email === '' || password === '') {
+//     res.status(400).send('Both email and password cannot be empty');
+//   } else {
+//     try {
+//       const user = await userDAO.getUser(email);
+//       if (user) {
+//         res.status(409).send('Email already signed up');
+//       } else {
+//         const encryptedPassword = await bcrypt.hash(password, 10);
+//         const userInfo = ({ email, password: encryptedPassword });
+//         const savedUser = await userDAO.createUser(userInfo);
+//         res.json(savedUser);
+//       }
+//     } catch (e) {
+//       next(e);
+//     }
+//   }
+// });
 
 
 // // Login: POST /login - Finds the user with the provided email.
